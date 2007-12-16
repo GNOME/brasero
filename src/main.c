feb3fa72551a44da521c3e52fdb26c71968ff98a
Philippe Rouquier <philippr@src.gnome.org>
2007-12-16 19:05:57 +0000

    fix #503661 – Wrong FSF address (patch by Bob Mauchin)
    	* src/main.c: (on_about_cb):
    
    svn path=/trunk/; revision=531

--- src/main.c
+++ src/main.c
@@ -243,7 +243,7 @@ on_about_cb (GtkAction *action, BraseroApp *app)
                 N_("You should have received a copy of the GNU General "
 		   "Public License along with Brasero; if not, write "
 		   "to the Free Software Foundation, Inc., "
-                   "59 Temple Place, Suite 330, Boston, MA  02111-1307  USA"),
+                   "51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA"),
 		NULL
         };
 
