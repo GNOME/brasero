df7a375aa0f093e851fb4107d3d0fe814e8a462c
Philippe Rouquier <philippr@src.gnome.org>
2008-10-18 18:20:14 +0000

    Remove leftover debug statements
    	* src/brasero-disc-option-dialog.c
    	(brasero_disc_option_dialog_load_multi_state),
    	(brasero_disc_option_dialog_update_multi):
    
    svn path=/trunk/; revision=1395

--- src/brasero-disc-option-dialog.c
+++ src/brasero-disc-option-dialog.c
@@ -92,11 +92,9 @@ brasero_disc_option_dialog_load_multi_state (BraseroDiscOptionDialog *dialog)
 	gboolean value;
 
 	priv = BRASERO_DISC_OPTION_DIALOG_PRIVATE (dialog);
-g_print ("KKLS\n");
-	session = brasero_burn_options_get_session (BRASERO_BURN_OPTIONS (dialog));
 
+	session = brasero_burn_options_get_session (BRASERO_BURN_OPTIONS (dialog));
 	if (!brasero_session_cfg_is_supported (BRASERO_SESSION_CFG (session), BRASERO_BURN_FLAG_MULTI)) {
-g_print ("RRKE\n");
 		gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (priv->multi_toggle), FALSE);
 		gtk_widget_set_sensitive (priv->multi_toggle, FALSE);
 		g_object_unref (session);
@@ -110,7 +108,7 @@ g_print ("RRKE\n");
 		g_object_unref (session);
 		return;
 	}
-g_print ("jlskjd\n");
+
 	/* set sensitivity */
 	value = brasero_session_cfg_is_compulsory (BRASERO_SESSION_CFG (session),
 						   BRASERO_BURN_FLAG_MULTI);
@@ -198,7 +196,7 @@ brasero_disc_option_dialog_update_multi (BraseroDiscOptionDialog *dialog)
 
 	session = brasero_burn_options_get_session (BRASERO_BURN_OPTIONS (dialog));
 	brasero_burn_session_get_input_type (session, &input);
-g_print ("REACHED\n");
+
 	/* MULTI and Video projects don't get along */
 	if (input.type == BRASERO_TRACK_TYPE_DATA
 	&& (input.subtype.fs_type & BRASERO_IMAGE_FS_VIDEO)
