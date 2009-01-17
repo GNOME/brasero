f8f73f546d8026fad2425e43392c723083a144b0
Philippe Rouquier <ykw@localhost.localdomain>
2009-01-17 19:46:27 +0000

    Relicenced to GPL 2 + restrictions
    2009-01-17  Philippe Rouquier  <ykw@localhost.localdomain>
    
    	Relicenced to GPL 2 + restrictions
    
    	* libbrasero-media/brasero-drive-selection.c:
    	* libbrasero-media/brasero-drive-selection.h:
    	* libbrasero-media/brasero-drive.c:
    	* libbrasero-media/brasero-drive.h:
    	* libbrasero-media/brasero-gio-operation.c:
    	* libbrasero-media/brasero-gio-operation.h:
    	* libbrasero-media/brasero-media-private.h:
    	* libbrasero-media/brasero-media.c:
    	* libbrasero-media/brasero-media.h.in:
    	* libbrasero-media/brasero-medium-monitor.c:
    	* libbrasero-media/brasero-medium-monitor.h:
    	* libbrasero-media/brasero-medium-selection-priv.h:
    	* libbrasero-media/brasero-medium-selection.c:
    	* libbrasero-media/brasero-medium-selection.h:
    	* libbrasero-media/brasero-medium.c:
    	* libbrasero-media/brasero-medium.h:
    	* libbrasero-media/brasero-units.c:
    	* libbrasero-media/brasero-units.h:
    	* libbrasero-media/brasero-volume.c:
    	* libbrasero-media/brasero-volume.h:
    	* libbrasero-media/burn-hal-watch.c:
    	* libbrasero-media/burn-hal-watch.h:
    	* libbrasero-media/burn-iso-field.c:
    	* libbrasero-media/burn-iso-field.h:
    	* libbrasero-media/burn-iso9660.c:
    	* libbrasero-media/burn-iso9660.h:
    	* libbrasero-media/burn-susp.c:
    	* libbrasero-media/burn-susp.h:
    	* libbrasero-media/burn-volume-source.c:
    	* libbrasero-media/burn-volume-source.h:
    	* libbrasero-media/burn-volume.c:
    	* libbrasero-media/burn-volume.h:
    	* libbrasero-media/scsi-base.h:
    	* libbrasero-media/scsi-command.h:
    	* libbrasero-media/scsi-device.h:
    	* libbrasero-media/scsi-dvd-structures.h:
    	* libbrasero-media/scsi-eject.c:
    	* libbrasero-media/scsi-error.c:
    	* libbrasero-media/scsi-error.h:
    	* libbrasero-media/scsi-get-configuration.c:
    	* libbrasero-media/scsi-get-configuration.h:
    	* libbrasero-media/scsi-get-performance.c:
    	* libbrasero-media/scsi-get-performance.h:
    	* libbrasero-media/scsi-mech-status.c:
    	* libbrasero-media/scsi-mech-status.h:
    	* libbrasero-media/scsi-mmc1.h:
    	* libbrasero-media/scsi-mmc2.h:
    	* libbrasero-media/scsi-mmc3.h:
    	* libbrasero-media/scsi-mode-pages.h:
    	* libbrasero-media/scsi-mode-select.c:
    	* libbrasero-media/scsi-mode-sense.c:
    	* libbrasero-media/scsi-opcodes.h:
    	* libbrasero-media/scsi-q-subchannel.h:
    	* libbrasero-media/scsi-read-capacity.c:
    	* libbrasero-media/scsi-read-capacity.h:
    	* libbrasero-media/scsi-read-cd.c:
    	* libbrasero-media/scsi-read-cd.h:
    	* libbrasero-media/scsi-read-disc-info.c:
    	* libbrasero-media/scsi-read-disc-info.h:
    	* libbrasero-media/scsi-read-disc-structure.c:
    	* libbrasero-media/scsi-read-disc-structure.h:
    	* libbrasero-media/scsi-read-format-capacities.c:
    	* libbrasero-media/scsi-read-format-capacities.h:
    	* libbrasero-media/scsi-read-toc-pma-atip.c:
    	* libbrasero-media/scsi-read-toc-pma-atip.h:
    	* libbrasero-media/scsi-read-track-information.c:
    	* libbrasero-media/scsi-read-track-information.h:
    	* libbrasero-media/scsi-read10.c:
    	* libbrasero-media/scsi-sbc.h:
    	* libbrasero-media/scsi-sense-data.c:
    	* libbrasero-media/scsi-sense-data.h:
    	* libbrasero-media/scsi-sg.c:
    	* libbrasero-media/scsi-spc1.h:
    	* libbrasero-media/scsi-status-page.h:
    	* libbrasero-media/scsi-test-unit-ready.c:
    	* libbrasero-media/scsi-utils.h:
    	* libbrasero-media/scsi-write-page.h:
    
    svn path=/trunk/; revision=1772

--- libbrasero-media/brasero-drive-selection.c
+++ libbrasero-media/brasero-drive-selection.c
@@ -1,24 +1,32 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 8; tab-width: 8 -*- */
 /*
- * brasero
- * Copyright (C) Philippe Rouquier 2005-2008 <bonfire-app@wanadoo.fr>
+ * Brasero
+ * Copyright (C) Philippe Rouquier 2005-2009 <bonfire-app@wanadoo.fr>
+ *
+ * Brasero is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The Brasero authors hereby grant permission for non-GPL compatible
+ * GStreamer plugins to be used and distributed together with GStreamer
+ * and Brasero. This permission is above and beyond the permissions granted
+ * by the GPL license by which Brasero is covered. If you modify this code
+ * you may extend this exception to your version of the code, but you are not
+ * obligated to do so. If you do not wish to do so, delete this exception
+ * statement from your version.
  * 
- *  Brasero is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * Brasero is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
  * 
- * brasero is distributed in the hope that it will be useful,
-   * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- * 
-   * You should have received a copy of the GNU General Public License
- * along with brasero.  If not, write to:
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to:
  * 	The Free Software Foundation, Inc.,
  * 	51 Franklin Street, Fifth Floor
  * 	Boston, MA  02110-1301, USA.
-   */
+ */
   
 #ifdef HAVE_CONFIG_H
 #  include <config.h>
--- libbrasero-media/brasero-volume.c
+++ libbrasero-media/brasero-volume.c
@@ -1,12 +1,20 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 8; tab-width: 8 -*- */
 /*
- * brasero
- * Copyright (C) Philippe Rouquier 2005-2008 <bonfire-app@wanadoo.fr>
+ * Brasero
+ * Copyright (C) Philippe Rouquier 2005-2009 <bonfire-app@wanadoo.fr>
  *
  * Brasero is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
+ *
+ * The Brasero authors hereby grant permission for non-GPL compatible
+ * GStreamer plugins to be used and distributed together with GStreamer
+ * and Brasero. This permission is above and beyond the permissions granted
+ * by the GPL license by which Brasero is covered. If you modify this code
+ * you may extend this exception to your version of the code, but you are not
+ * obligated to do so. If you do not wish to do so, delete this exception
+ * statement from your version.
  * 
  * Brasero is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,8 +22,10 @@
  * GNU Library General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
+ * along with this program; if not, write to:
+ * 	The Free Software Foundation, Inc.,
+ * 	51 Franklin Street, Fifth Floor
+ * 	Boston, MA  02110-1301, USA.
  */
 
 #ifdef HAVE_CONFIG_H
