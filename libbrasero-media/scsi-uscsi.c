b8ec7b3ce32700c18db6761d7c8a709854a1d7b4
Philippe Rouquier <ykw@localhost.localdomain>
2009-01-22 16:58:40 +0000

    Modified the licence of the last file that was GPL2 only with author's
    2009-01-22  Philippe Rouquier  <ykw@localhost.localdomain>
    
    	Modified the licence of the last file that was GPL2 only with author's
    	agreement
    
    	* libbrasero-media/scsi-uscsi.c:
    
    svn path=/trunk/; revision=1806

--- libbrasero-media/scsi-uscsi.c
+++ libbrasero-media/scsi-uscsi.c
@@ -1,16 +1,20 @@
-/***************************************************************************
- *            scsi-uscsi.c
- *
- *  Wed Oct 18 14:39:28 2008
- *  Copyright  2008  Lin Ma
- *  <lin.ma@sun.com>
- ****************************************************************************/
-
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 8; tab-width: 8 -*- */
 /*
+ * Libbrasero-media
+ * Copyright (C) Lin Ma 2008 <lin.ma@sun.com>
+ *
  * Libbrasero-media is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
+ *
+ * The Libbrasero-media authors hereby grant permission for non-GPL compatible
+ * GStreamer plugins to be used and distributed together with GStreamer
+ * and Libbrasero-media. This permission is above and beyond the permissions granted
+ * by the GPL license by which Libbrasero-media is covered. If you modify this code
+ * you may extend this exception to your version of the code, but you are not
+ * obligated to do so. If you do not wish to do so, delete this exception
+ * statement from your version.
  * 
  * Libbrasero-media is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -18,8 +22,10 @@
  * GNU Library General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
+ * along with this program; if not, write to:
+ * 	The Free Software Foundation, Inc.,
+ * 	51 Franklin Street, Fifth Floor
+ * 	Boston, MA  02110-1301, USA.
  */
 
 #ifdef HAVE_CONFIG_H
