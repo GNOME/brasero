3d903933ca5649c4fdd56c82864ea53434a4225e
Philippe Rouquier <bonfire-app@wanadoo.fr>
2009-05-09 14:24:00 +0200

    Revert change e3ab54dd6cd412ba5841ff333ebc1494a502fa65  (brasero option dialog made absolutely not resizable)

--- libbrasero-burn/brasero-burn-options.c
+++ libbrasero-burn/brasero-burn-options.c
@@ -468,7 +468,6 @@ static void
 brasero_burn_options_init (BraseroBurnOptions *object)
 {
 	gtk_dialog_set_has_separator (GTK_DIALOG (object), FALSE);
-	gtk_window_set_resizable (GTK_WINDOW (object), FALSE);
 	gtk_window_set_icon_name (GTK_WINDOW (object), "brasero");
 }
 
