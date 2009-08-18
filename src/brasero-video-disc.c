19d8084927b60d9be4f0a14daf461ea63dc3d5a6
Philippe Rouquier <bonfire-app@wanadoo.fr>
2009-08-18 13:40:45 +0200

    Ensure help message is properly displayed for video projects

--- src/brasero-video-disc.c
+++ src/brasero-video-disc.c
@@ -1365,7 +1365,7 @@ brasero_video_disc_is_empty (BraseroDisc *disc)
 	priv = BRASERO_VIDEO_DISC_PRIVATE (disc);
 	model = gtk_tree_view_get_model (GTK_TREE_VIEW (priv->tree));
 	if (!model)
-		return TRUE;
+		return FALSE;
 
 	return gtk_tree_model_iter_n_children (model, NULL) != 0;
 }
