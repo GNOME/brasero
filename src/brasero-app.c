c94b8f25c0e37370b920080d3338a172983eac78
Philippe Rouquier <bonfire-app@wanadoo.fr>
2009-09-05 20:11:40 +0200

    Fixed a stacking problem.

--- src/brasero-app.c
+++ src/brasero-app.c
@@ -958,7 +958,6 @@ brasero_app_set_toplevel (BraseroApp *app, GtkWindow *window)
 	}
 	else {
 		gtk_window_set_transient_for (GTK_WINDOW (window), GTK_WINDOW (priv->mainwin));
-		gtk_window_set_modal (GTK_WINDOW (window), TRUE);
 
 		/* hide main dialog if it is shown */
 		gtk_widget_hide (GTK_WIDGET (priv->mainwin));
