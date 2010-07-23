GNOME/brasero

commit b1b8ae5c0bf918503cd10380c9634d814f9f7a23

Another try at #606010 - crashes at audio CD insertion

libbrasero-media/brasero-medium.c
@@ -2793,8 +2793,13 @@ brasero_medium_read_CD_TEXT (BraseroMedium *self,
 
 	/* Get the number of CD-Text Data Packs.
 	 * Some drives seem to report an idiotic cd_text->hdr->len. So use size
-	 * to be on the safe side. */
-	num = (size - sizeof (BraseroScsiTocPmaAtipHdr)) / sizeof (BraseroScsiCDTextPackData);
+	 * to be on a safer side. */
+	if (size < sizeof (BraseroScsiTocPmaAtipHdr)) {
+		g_free (cd_text);
+		return;
+	}
+
+	num = (num - sizeof (BraseroScsiTocPmaAtipHdr)) / sizeof (BraseroScsiCDTextPackData);
 	if (num <= 0) {
 		g_free (cd_text);
 		return;

commit 4a34a5129d0d9d3a2d155b9f0c53cb01c68f87c0

Fix error introduced by patch b1b8ae5c0bf918503cd10380c9634d814f9f7a23

libbrasero-media/brasero-medium.c
@@ -2799,7 +2799,7 @@ brasero_medium_read_CD_TEXT (BraseroMedium *self,
 		return;
 	}
 
-	num = (num - sizeof (BraseroScsiTocPmaAtipHdr)) / sizeof (BraseroScsiCDTextPackData);
+	num = (size - sizeof (BraseroScsiTocPmaAtipHdr)) / sizeof (BraseroScsiCDTextPackData);
 	if (num <= 0) {
 		g_free (cd_text);
 		return;