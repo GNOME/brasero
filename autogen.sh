8b6b00252f2987df7408cf8bc571b086e0e64786
John Cardullo <john@jcarmedia.org>
2026-07-13 00:38:45 -0700

    build: Modernize autogen.sh and drop deprecated gnome-common dependency
    Replaces the obsolete 'gnome-autogen.sh' wrapper with standard 'autoreconf'
    to bootstrap the build system. This aligns the repository with modern
    GNOME autotools recommendations.
    
    Additionally, this adds support for the NOCONFIGURE environment variable,
    allowing flatpak-builder and GNOME Builder to configure and build the
    project automatically without manual command-line intervention.

--- autogen.sh
+++ autogen.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
+set -e
 
-srcdir=`dirname $0`
+srcdir=`dirname "$0"`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="brasero"
 
-(test -f $srcdir/configure.ac \
-  && test -f $srcdir/README \
-  && test -d $srcdir/src) || {
+(test -f "$srcdir/configure.ac" \
+  && test -f "$srcdir/README" \
+  && test -d "$srcdir/src") || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
 }
 
-which gnome-autogen.sh || {
-    echo "You need to install gnome-common from the GNOME CVS"
-    exit 1
-}
-REQUIRED_AUTOMAKE_VERSION=1.7 USE_GNOME2_MACROS=1 . gnome-autogen.sh --enable-gtk-doc "$@"
+# Run standard autoreconf to generate build files
+autoreconf --force --install --verbose "$srcdir"
+
+# Run configure unless NOCONFIGURE is set (for tools like GNOME Builder / flatpak-builder)
+if [ -z "$NOCONFIGURE" ]; then
+    "$srcdir/configure" "$@"
+fi
